d450c89f3c7b3bfe78087cc36c658655208e3a24
Gabor Szabo <gabor@szabgab.com>
2015-01-07 13:49:35 +0200

    be able to serve regular pages

--- app.pl
+++ app.pl
@@ -1,29 +1,68 @@
 use v6;
 use Bailador;
 
-get '/' => sub {
-	'root';
-}
+use lib 'lib';
+
+use Perl6::Maven::Tools;
+use Perl6::Maven::Authors;
+use Perl6::Maven::Page;
+
+
+multi MAIN(
+	Str  :$source!,
+	) {
+
+	read_config($source);
+	my $authors = Perl6::Maven::Authors.new( source_dir => $source);
+	$authors.read_authors;
+
+
+	get '/' => sub {
+		'root';
+	}
 
 
 #get '/robots.txt' => sub {
 #	"Sitemap: http://perl6maven.com/sitemap.xml";
 #}
 
-#my $static_dir = '/home/gabor/work/perl6maven-live.com/
-my $static_dir = '/home/gabor/work/Perl6-Maven/files';
-my $config_file = '/home/gabor/work/perl6maven.com/config.ini';
-my $authors_file = '/home/gabor/work/perl6maven.com/authors.txt';
-my $pages_dir = '/home/gabor/work/perl6maven.com/pages';
-my $include_dir ='/home/gabor/work/perl6maven.com/files'; 
+	#my $root = $*CWD;
+	#if $*PROGRAM_NAME ~~ /\// {
+	#}
+	#my $$static_dir = "$root/files";
+	my $pages_dir = "$source/pages";
+	my $include_dir = "$source/files/";
 
-get / '/' (.+) / => sub ($file) {
-    if "$static_dir/$file".IO ~~ :e {
-		# TODO set content-type !
-    	return "$static_dir/$file".IO.slurp
+#	my %CT = (
+#		js  => 'application/javascript',
+#		css => 'text/css',
+#		png => 'image/png',
+#		ico => 'image/x-icon',
+#		gif => 'image/gif',
+#		jpg => 'image/jpeg',
+#	);
+
+	get / '/' (.+) / => sub ($file) {
+		#my $full_path = "$static_dir/$file";
+	    #if $full_path.IO ~~ :e {
+			# TODO set content-type !
+			#my $ext = $full_path ~~ /\.(<[a..z]>+)$/;
+			#if $ext and %CT{$ext} {
+			#	content_type(%CT{$ext});
+			#}
+			#my $out = open($full_path, :r).read($full_path.IO.s);
+			#return $out.Str;
+		#}
+		my $txt_file = "$source/pages/$file.txt";
+		#return $txt_file;
+		if $txt_file.IO ~~ :e {
+			my $page = Perl6::Maven::Page.new(authors => $authors.authors, include => $include_dir, outdir => '');
+			$page.read_file($txt_file, '');
+			return $page.generate;
+		}
+		return 'Not found';
 	}
-	return 'Not found';
+
+	baile;
 }
 
-
-baile;
